17bb1a3fdb67fd4ca53ca9ae30b7acd2055d0177
admin <alexander.sviridof@gmail.com>
2023-05-04 20:10:02 +0300

    ДЗ 1

--- Assets/Scripts/Unit.cs
+++ Assets/Scripts/Unit.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Threading.Tasks;
 
 public class Unit : MonoBehaviour
 {
@@ -22,7 +23,7 @@ public class Unit : MonoBehaviour
             if(newHealth >= 100) {
                 health = 100;
                 Debug.Log("health=" + health.ToString());
-                break;
+                yield break;
             } else 
             {
                 health = newHealth;
@@ -32,6 +33,19 @@ public class Unit : MonoBehaviour
         }
     }
 
+    async Task Task1()
+    {
+        await Task.Delay(1000);
+        Debug.Log("Task1 end");
+    }
+
+    async Task Task2()
+    {
+        for(int i = 60; i != 0; --i) {
+            await Task.Yield();    
+        }
+        Debug.Log("Task2 end");
+    }
 
     // Start is called before the first frame update
     void Start()
