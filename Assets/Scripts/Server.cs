e867c16aa9c0cbadaae716178bcc3bdf2103f1d2
admin <alexander.sviridof@gmail.com>
2023-05-11 19:52:49 +0300

    ДЗ 3

---
+++ Assets/Scripts/Server.cs
@@ -0,0 +1,109 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class Server : MonoBehaviour
+{
+    class CustomConnnection {
+        public int id;
+        public string name;
+    }
+
+    private const int MAX_CONNECTION = 10;
+    private int port = 5805;
+    private int hostID;
+    private int reliableChannel;
+    private bool isStarted = false;
+    private byte error;
+    List<CustomConnnection> connections = new List<CustomConnnection>();
+    public void StartServer()
+    {
+        NetworkTransport.Init();
+        ConnectionConfig cc = new ConnectionConfig();
+        // cc.ConnectTimeout = 500;
+        // cc.MaxConnectionAttempt = 2;
+
+        reliableChannel = cc.AddChannel(QosType.Reliable);
+        HostTopology topology = new HostTopology(cc, MAX_CONNECTION);
+        hostID = NetworkTransport.AddHost(topology, port);
+        isStarted = true;
+    }
+    public void ShutDownServer()
+    {
+        if (!isStarted) return;
+        NetworkTransport.RemoveHost(hostID);
+        NetworkTransport.Shutdown();
+        isStarted = false;
+    }
+    void Update()
+    {
+        if (!isStarted) return;
+        int recHostId;
+        int connectionId;
+        int channelId;
+        byte[] recBuffer = new byte[1024];
+        int bufferSize = 1024;
+        int dataSize;
+        NetworkEventType recData = NetworkTransport.Receive(out recHostId, out connectionId, out
+        channelId, recBuffer, bufferSize, out dataSize, out error);
+        while (recData != NetworkEventType.Nothing)
+        {
+            switch (recData)
+            {
+                case NetworkEventType.Nothing:
+                    break;
+                case NetworkEventType.ConnectEvent:
+                    connections.Add(new CustomConnnection() {id = connectionId});
+                    SendMessageToAll($"Player {connectionId} has connected.");
+                    Debug.Log($"Player {connectionId} has connected.");
+                    break;
+                case NetworkEventType.DataEvent: {
+                    string message = Encoding.Unicode.GetString(recBuffer, 0, dataSize);
+
+                    var conn = connections.Find(connection => connection.id == connectionId);
+                    if(string.IsNullOrEmpty(conn.name)) {
+                        conn.name = message;
+                        SendMessageToAll($"Player {conn.name}: присоединился");
+                        Debug.Log($"Player {conn.name}: присоединился");
+                        break;
+                    }
+
+                    SendMessageToAll($"Player {conn.name}: {message}");
+                    Debug.Log($"Player {conn.name}: {message}");
+                    break;
+                }
+                case NetworkEventType.DisconnectEvent:
+                {
+                    var conn = connections.Find(connection => connection.id == connectionId);
+                    var name = conn.name;
+
+                    if(conn != null)
+                        connections.Remove(conn);
+
+                    SendMessageToAll($"Player {name} has disconnected.");
+                    Debug.Log($"Player {name} has disconnected.");
+                    break;
+                }
+                case NetworkEventType.BroadcastEvent:
+                    break;
+            }
+            recData = NetworkTransport.Receive(out recHostId, out connectionId, out channelId, recBuffer,
+            bufferSize, out dataSize, out error);
+        }
+    }
+    public void SendMessageToAll(string message)
+    {
+        for (int i = 0; i < connections.Count; i++)
+        {
+            SendMessage(message, connections[i].id);
+        }
+    }
+    public void SendMessage(string message, int connectionID)
+    {
+        byte[] buffer = Encoding.Unicode.GetBytes(message);
+        NetworkTransport.Send(hostID, connectionID, reliableChannel, buffer, message.Length * sizeof(char), out error);
+        if ((NetworkError)error != NetworkError.Ok) Debug.Log((NetworkError)error);
+    }
+}
---
+++ Assets/Scripts/UIController.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIController : MonoBehaviour
+{
+    [SerializeField]
+    private Button buttonStartServer;
+    [SerializeField]
+    private Button buttonShutDownServer;
+    [SerializeField]
+    private Button buttonConnectClient;
+    [SerializeField]
+    private Button buttonDisconnectClient;
+    [SerializeField]
+    private Button buttonSendMessage;
+    [SerializeField]
+    private TMP_InputField inputField;
+    [SerializeField]
+    private TextField textField;
+    [SerializeField]
+    private Server server;
+    [SerializeField]
+    private Client client;
+    private void Start()
+    {
+        buttonStartServer.onClick.AddListener(() => StartServer());
+        buttonShutDownServer.onClick.AddListener(() => ShutDownServer());
+        buttonConnectClient.onClick.AddListener(() => Connect());
+        buttonDisconnectClient.onClick.AddListener(() => Disconnect());
+        buttonSendMessage.onClick.AddListener(() => SendMessage());
+        client.onMessageReceive += ReceiveMessage;
+    }
+    private void StartServer()
+    {
+        server.StartServer();
+    }
+    private void ShutDownServer()
+    {
+        server.ShutDownServer();
+
+    }
+    private void Connect()
+    {
+        client.Connect();
+    }
+    private void Disconnect()
+    {
+        client.Disconnect();
+    }
+    private void SendMessage()
+    {
+        client.SendMessage(inputField.text);
+        inputField.text = "";
+    }
+    public void ReceiveMessage(object message)
+    {
+        textField.ReceiveMessage(message);
+    }
+
+}
