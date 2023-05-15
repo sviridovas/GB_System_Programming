3c8908623134aa8893835dff0b23b94510bfa213
admin <alexander.sviridof@gmail.com>
2023-05-15 19:46:56 +0300

    ДЗ 4

---
+++ Assets/Scripts/TestCoroutine.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestCoroutine : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(MyCour());
+    }
+
+    IEnumerator MyCour()
+    {
+        Debug.Log("Before yield");
+        yield return null;        
+        Debug.Log("After yield");
+    }
+}
